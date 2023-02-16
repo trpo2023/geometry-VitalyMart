b26d7568fa04a712450b23eba2c8bf62c2ca2f73
VitalyMart <vitaliy2004mart@mail.ru>
2023-02-16 12:10:16 +0700

    Edit Makefile and main.c

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ all: main
 main: main.c
 	gcc -Wall -Werror -o main main.c
 run:
-	./main
+	./main test.txt
--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(){
-printf("hello world");
-return 0;
+
+int main(int argc, char* argv[])
+{ 
+  FILE *f;
+  char buffer[256];
+  f = fopen(argv[1], "r");
+  while((fgets(buffer, 256, f))!=NULL)
+        {
+            printf("%s", buffer);
+        }
+        fclose(f);
+  
+    return 0;
 }
